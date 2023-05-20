a7f20eb37a0cd92b941620d6abe5587159ddb34b
nathantipton <ntipton21@gmail.com>
2023-05-19 23:29:02 -0400

    Fix

--- src/routes/resume/+page.server.ts
+++ src/routes/resume/+page.server.ts
@@ -1,4 +1,4 @@
-import { RESUME_V1 } from '../../../../../../../Users/ntipt/dev/resume/src/lib/resumes/v1';
+import { RESUME_V1 } from '$lib/resumes/v1';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
