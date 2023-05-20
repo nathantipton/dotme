ec10a252dd8932d6fbc4bcd33d641bc07abb6c2d
nathantipton <ntipton21@gmail.com>
2023-05-20 10:04:19 -0400

    style updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,14 +29,20 @@
 	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
 </svelte:head>
 
-<div class="dark:bg-zinc-950 content min-h-safe min-w-full">
+<div class="dark:bg-zinc-950 bg-gradient min-h-safe min-w-full">
 	<slot />
 </div>
 
 <style lang="postcss">
-	.content {
+	:global(.dark .bg-gradient) {
 		background-color: hsla(240, 10%, 3%, 1);
 		background-image: radial-gradient(at 63% 65%, hsla(17, 87%, 40%, 0.09) 0px, transparent 50%),
 			radial-gradient(at 40% 50%, hsla(240, 3%, 15%, 0.48) 0px, transparent 50%);
+	}
+
+	:global(.bg-gradient) {
+		background-color: hsla(0, 0%, 98%, 1);
+		background-image: radial-gradient(at 63% 65%, hsla(17, 34%, 61%, 0.192) 0px, transparent 50%),
+			radial-gradient(at 40% 50%, hsla(240, 7%, 70%, 0.226) 0px, transparent 50%);
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,40 @@
-<div class="content mx-auto flex h-screen max-w-2xl flex-col items-stretch justify-center gap-4">
+<div class="content mx-auto flex h-safe max-w-2xl flex-col items-stretch justify-center gap-4">
 	<nav class="absolute top-0 right-0 p-8">
 		<ul class="flex flex-row items-center justify-end gap-4">
 			<li>
-				<a href="/resume" class="text-zinc-400 underline-offset-8 hover:text-white hover:underline"
+				<a
+					href="/resume"
+					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
 					>Resume</a
 				>
 			</li>
 			<li>
 				<a
 					href="/projects"
-					class="text-zinc-400 underline-offset-8 hover:text-white hover:underline">Projects</a
+					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
+					>Projects</a
 				>
 			</li>
 			<li>
 				<a
 					href="https://www.linkedin.com/in/tntipton/"
 					target="_blank"
-					class="text-zinc-400 underline-offset-8 hover:text-white hover:underline">LinkedIn</a
+					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
+					>LinkedIn</a
 				>
 			</li>
 			<li>
 				<a
 					href="https://github.com/nathantipton"
 					target="_blank"
-					class="text-zinc-400 underline-offset-8 hover:text-white hover:underline">GitHub</a
+					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
+					>GitHub</a
 				>
 			</li>
 		</ul>
 	</nav>
 	<div
-		class="flex flex-row items-center justify-start gap-6
+		class="flex flex-row items-center justify-center gap-4 md:justify-start md:gap-6
 	"
 	>
 		<img
@@ -38,9 +43,8 @@
 			src="https://media.licdn.com/dms/image/C4E03AQGABNCNxkuHWA/profile-displayphoto-shrink_800_800/0/1586179044008?e=1689811200&v=beta&t=_6QvZ_Vb7ZexNEE5DnRP0RwiI3T5qrbZ-kmCs5QA0xE"
 		/>
 		<div>
-			<h1 class="text-5xl font-bold tracking-wide">Nathan Tipton</h1>
-			<h4 class="text-lg font-semibold dark:text-zinc-500">Web Developer &amp; Designer</h4>
+			<h1 class="text-2xl font-bold tracking-wide md:text-5xl">Nathan Tipton</h1>
+			<h4 class="font-semibold dark:text-zinc-500 md:text-lg">Web Developer &amp; Designer</h4>
 		</div>
 	</div>
 </div>
-
