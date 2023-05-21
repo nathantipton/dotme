226e27535a1d2e35173e61a9bfadc18c59ff8b8a
nathantipton <ntipton21@gmail.com>
2023-05-20 21:41:04 -0400

    metatags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,9 +27,28 @@
 
 <svelte:head>
 	{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}
+	<meta property="og:title" content="Nathan Tipton - Home" />
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:description"
+		content="Creative and ambitious software developer with ten years of demonstrated experience in the
+        development, design, implementation, and maintenance of modern web applications. Highly
+        effective at understanding business needs and providing innovative solutions."
+	/>
+	<meta property="og:url" content="https://nathantipton.me" />
+	<meta property="og:image" content="profile.jpg" />
+	<meta property="og:image:alt" content="Nathan Tipton - Home" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@ntipton21" />
+	<meta name="twitter:title" content="Nathan Tipton - Resume" />
+	<meta name="twitter:description" content="Creative and ambitious software developer with ten years of demonstrated experience in the
+        development, design, implementation, and maintenance of modern web applications. Highly
+        effective at understanding business needs and providing innovative solutions." />
+	<meta name="twitter:image" content="profile.jpg" />
+	<meta name="twitter:image:alt" content="Nathan Tipton - Resume" />
 </svelte:head>
 
-<div class="dark:bg-zinc-950 bg-gradient min-h-safe min-w-full">
+<div class="bg-gradient min-h-safe min-w-full dark:bg-zinc-950">
 	<slot />
 </div>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <div class="content mx-auto flex h-safe max-w-2xl flex-col items-stretch justify-center gap-4">
-	<nav class="absolute top-0 right-0 p-8">
+	<nav class="absolute right-0 top-0 p-8">
 		<ul class="flex flex-row items-center justify-end gap-4">
 			<li>
 				<a
 					href="/experience"
-					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
+					class="text-zinc-500 underline-offset-8 hover:text-zinc-950 hover:underline dark:text-zinc-400 dark:hover:text-white"
 					>Experience</a
 				>
 			</li>
 			<li>
 				<a
 					href="/projects"
-					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
+					class="text-zinc-500 underline-offset-8 hover:text-zinc-950 hover:underline dark:text-zinc-400 dark:hover:text-white"
 					>Projects</a
 				>
 			</li>
@@ -19,7 +19,7 @@
 				<a
 					href="https://www.linkedin.com/in/tntipton/"
 					target="_blank"
-					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
+					class="text-zinc-500 underline-offset-8 hover:text-zinc-950 hover:underline dark:text-zinc-400 dark:hover:text-white"
 					>LinkedIn</a
 				>
 			</li>
@@ -27,7 +27,7 @@
 				<a
 					href="https://github.com/nathantipton"
 					target="_blank"
-					class="hover:text-zinc-950 text-zinc-500 underline-offset-8 hover:underline dark:text-zinc-400 dark:hover:text-white"
+					class="text-zinc-500 underline-offset-8 hover:text-zinc-950 hover:underline dark:text-zinc-400 dark:hover:text-white"
 					>GitHub</a
 				>
 			</li>
@@ -37,11 +37,7 @@
 		class="flex flex-row items-center justify-center gap-4 md:justify-start md:gap-6
 	"
 	>
-		<img
-			class="h-24 w-24 rounded-full"
-			alt="Nathan Tipton"
-			src="https://media.licdn.com/dms/image/C4E03AQGABNCNxkuHWA/profile-displayphoto-shrink_800_800/0/1586179044008?e=1689811200&v=beta&t=_6QvZ_Vb7ZexNEE5DnRP0RwiI3T5qrbZ-kmCs5QA0xE"
-		/>
+		<img class="h-24 w-24 rounded-full" alt="Nathan Tipton" src="profile.jpg" />
 		<div>
 			<h1 class="text-2xl font-bold tracking-wide md:text-5xl">Nathan Tipton</h1>
 			<h4 class="font-semibold dark:text-zinc-500 md:text-lg">Web Developer &amp; Designer</h4>
