e054f6f9308eb5adb617b4664564a0388a4fe646
nathantipton <ntipton21@gmail.com>
2023-12-13 14:12:01 -0500

    filter active projects

--- src/routes/projects/+page.server.ts
+++ src/routes/projects/+page.server.ts
@@ -5,6 +5,9 @@ export const load: PageServerLoad = async () => {
 
     const getProjects = async () => {
         return prisma.project.findMany({
+            where: {
+                is_active: true
+            },
             orderBy: {
                 sort: "asc"
             }
